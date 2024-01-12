ef2a978184344f17c4cc9b17c478b70ee9c81a73
Маргарита Шайдурова <shaidrv@gmail.com>
2024-01-12 20:17:23 +0700

    fixes

--- src/wallet_address_validator.js
+++ src/wallet_address_validator.js
@@ -1,25 +1,35 @@
-var currencies = require('./currencies');
+if (!global.Buffer) {
+    global.Buffer = require("buffer").Buffer;
+}
 
-var DEFAULT_CURRENCY_NAME = 'bitcoin';
+var currencies = require("./currencies");
+
+var DEFAULT_CURRENCY_NAME = "bitcoin";
 
 module.exports = {
     //validate: function (address, currencyNameOrSymbol, networkType) {
     validate: function (address, currencyNameOrSymbol, opts) {
-        var currency = currencies.getByNameOrSymbol(currencyNameOrSymbol || DEFAULT_CURRENCY_NAME);
+        var currency = currencies.getByNameOrSymbol(
+            currencyNameOrSymbol || DEFAULT_CURRENCY_NAME
+        );
 
         if (currency && currency.validator) {
-            if (opts && typeof opts === 'string') {
-                return currency.validator.isValidAddress(address, currency, { networkType: opts });
+            if (opts && typeof opts === "string") {
+                return currency.validator.isValidAddress(address, currency, {
+                    networkType: opts,
+                });
             }
             return currency.validator.isValidAddress(address, currency, opts);
         }
 
-        throw new Error('Missing validator for currency: ' + currencyNameOrSymbol);
+        throw new Error(
+            "Missing validator for currency: " + currencyNameOrSymbol
+        );
     },
     getCurrencies: function () {
         return currencies.getAll();
     },
-    findCurrency: function(symbol) {
-        return currencies.getByNameOrSymbol(symbol) || null ;
-    }
+    findCurrency: function (symbol) {
+        return currencies.getByNameOrSymbol(symbol) || null;
+    },
 };
